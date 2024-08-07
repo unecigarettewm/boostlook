1835cd314607a0d8dcf286f0111f5511c4058dd4
Vinnie Falco <4302819+unecigarettewm@users.noreply.github.com>
2024-08-07 14:41:54 -0700

    layout

--- boostlook.rb
+++ boostlook.rb
@@ -3,6 +3,7 @@ Asciidoctor::Extensions.register do
     process do |doc, output|
       output = output.sub(/<body (.*?)>/, '<body \\1><div class="doc">')
       output = output.sub(/<\/body>/, "</div></body>")
+      #output = output.sub(/<\/body>/, "</body>")
     end
   end
 end
